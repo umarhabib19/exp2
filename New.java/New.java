6b45dfa6bc7d135649bd54ccce8a76284024f0e2
umarhabib19 <2025cs_umarfaridahmedhabib_h@nie.ac.in>
2026-09-06 09:43:09 +0000

    a java file

---
+++ New.java/New.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class New{
+public static void main(string[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter your input\n");
+String hello = sc.nextLine();
+}
+}           
